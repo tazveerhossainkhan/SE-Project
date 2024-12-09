dfac3e1c35fc606fe7da43d0fb88e245f284d76a
tazveerhossainkhan <tazveerhossainkhan009@gmail.com>
2024-12-09 22:10:22 +0600

    Remove item from cart, clear cart functionality done

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -3,6 +3,7 @@ import App from "../App";
 import Home from "../pages/home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import CartPage from "../pages/books/CartPage";
 
 const router = createBrowserRouter([
     {
@@ -28,6 +29,13 @@ const router = createBrowserRouter([
         {
           path: "/register",
           element: <Register/>
+        },
+        {
+          path: "/cart",
+          element: <CartPage/>
+        },
+        {
+          path: "/",
         }
       ]
     },
