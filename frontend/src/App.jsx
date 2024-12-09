90231456e13ae226b5d6a27b00b00f65aeb6d48e
tazveerhossainkhan <tazveerhossainkhan009@gmail.com>
2024-12-09 11:29:21 +0600

    Font and colors added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
     <navbar>Navbar</navbar>
-      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6'>
+      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
       <Outlet/>
       </main>
     <footer>Footer</footer>
