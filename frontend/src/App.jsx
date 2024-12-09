a2fecc8981957806385212c6694ab85d8db0dd48
tazveerhossainkhan <tazveerhossainkhan009@gmail.com>
2024-12-09 09:25:55 +0600

    Environment Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+
+function App() {
+  
+
+  return (
+    <>
+    <navbar>Navbar</navbar>
+      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6'>
+      <Outlet/>
+      </main>
+    <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App
