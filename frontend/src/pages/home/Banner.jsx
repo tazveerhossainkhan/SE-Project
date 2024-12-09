f9a8c897fd239827b548b7b71f219611da67f6b9
sanjida-alam <taungona202243@bscse.uiu.ac.bd>
2024-12-09 15:21:54 +0600

    Modify file for banner

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import bannerImg from "../../assets/pet.png"
+
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className='flex flex-col md:flex-row-reverse py-16 justify-between items-center gap-12'>
+         <div className='md:w-1/2 w-full flex items-center md:justify-end'>
+            <img src={bannerImg} alt="" /> 
+        </div>
+        
+        <div className='md:w-1/2 w-full'>
+            <h1 className='md:text-5xl text-2xl font-medium mb-7'>From Tiny Paws to Big Adventures, Your Perfect Pet Awaits!</h1>
+            <p className='mb-10'>
+            It's time to welcome a new furry friend into your life! From playful puppies to cuddly kittens, our latest arrivals feature the perfect companions for every home. Start your journey to unconditional love today!</p>
+
+            <button className='btn-primary'>Subscribe</button>
+        </div>
+
+       
+    </div>
   )
 }
 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
 
 import bannerImg from "../../assets/pet.png"
+import Banner from './Banner'
 
-const Banner = () => {
+const Home = () => {
   return (
-    <div className='flex flex-col md:flex-row-reverse py-16 justify-between items-center gap-12'>
-         <div className='md:w-1/2 w-full flex items-center md:justify-end'>
-            <img src={bannerImg} alt="" />
-        </div>
-        
-        <div className='md:w-1/2 w-full'>
-            <h1 className='md:text-5xl text-2xl font-medium mb-7'>From Tiny Paws to Big Adventures, Your Perfect Pet Awaits!</h1>
-            <p className='mb-10'>
-            It's time to welcome a new furry friend into your life! From playful puppies to cuddly kittens, our latest arrivals feature the perfect companions for every home. Start your journey to unconditional love today!</p>
-
-            <button className='btn-primary'>Subscribe</button>
-        </div>
-
-       
-    </div>
+    <Banner/>
+    
   )
 }
 
